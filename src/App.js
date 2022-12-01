213ee432c64961d18d495a5e59a1f97f5994da95
beka <bekalegesse9@gmail.com>
2022-12-01 14:07:24 +0300

    navbar update

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ import './App.css'
 const App = () => {
   return (
     <div className="App">
-        <div className="gradient__bg">
-          <Navbar />
-          <Header/>
-        </div>
-        <Brand/>
-        <WhatGPT3/>
-        <Features/>
-        <Possibility/>
-        <CTA/>
-        <Blog/>
-        <Footer/>
+      <div className="gradient__bg">
+        <Navbar />
+        <Header/>
+      </div>
+      <Brand/>
+      <WhatGPT3/>
+      <Features/>
+      <Possibility/>
+      <CTA/>
+      <Blog/>
+      <Footer/>
     </div>
   )
 }
